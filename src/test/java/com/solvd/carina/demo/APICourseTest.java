d64e71e096cabde4ab30586254b749f60ead4dc2
Atilio Boher <atilioboher@gmail.com>
2024-02-04 16:33:37 -0300

    Add post test and add index to get test

--- src/test/java/com/solvd/carina/demo/APICourseTest.java
+++ src/test/java/com/solvd/carina/demo/APICourseTest.java
@@ -1,6 +1,7 @@
 package com.solvd.carina.demo;
 
 import com.solvd.carina.demo.api.products.GetProduct;
+import com.solvd.carina.demo.api.products.PostProduct;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
 import org.testng.annotations.Test;
@@ -11,7 +12,17 @@ public class APICourseTest implements IAbstractTest {
     @MethodOwner(owner = "Atilio")
     public void getProductTest() {
         GetProduct getProduct = new GetProduct();
+        getProduct.setIndex(2);
         getProduct.callAPIExpectSuccess();
         getProduct.validateResponseAgainstSchema("api/products/_get/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "Atilio")
+    public void postProductTest() {
+        PostProduct postProduct = new PostProduct();
+        postProduct.setProperties("api/products/product.properties");
+        postProduct.callAPIExpectSuccess();
+        postProduct.validateResponse();
     }
 }
